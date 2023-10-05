9222f44aa3dcb2c957866de521df7c546b6f0e4c
hiroto <h.nishihara.sys23@morijyobi.ac.jp>
2023-10-05 10:45:38 +0900

    Hello World を１００解出力します

--- pra2.java
+++ pra2.java
@@ -1,6 +1,6 @@
 pubiric class practice{
 	pubric static void main (String[] args){
-		for (int cut = 0; cut <= 100; cut++)
-			System.out.println ("Hwllo World");
+		for (int cut = 1; cut <= 100; cut++)
+			System.out.println ("Hello World");
 	}
 }
